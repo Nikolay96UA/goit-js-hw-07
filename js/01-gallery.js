69ec3ec4c88ce6456e2e129c9f917610f908b997
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-23 15:56:49 +0200

    render page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+
+const addGalleryEl = function (galleryItems) {
+  return galleryItems
+    .map(({ preview, description }) => {
+      return `<li class = "gallery__item">
+    <img class = "gallery__image gallery__link" src="${preview}"
+    alt = "${description}">
+    </li>`;
+    })
+    .join("");
+};
+
+console.log(addGalleryEl(galleryItems));
+
+galleryBox.insertAdjacentHTML("afterbegin", addGalleryEl(galleryItems));
